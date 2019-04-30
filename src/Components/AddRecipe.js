920ce23b5154327c93df6885409c150d77ff4011
Peter Senn <psenn@vertafore.com>
2019-04-30 14:10:03 -0600

    form is set up, visually and functionally, onSubmit gets state which has everything for a new recipe, end point added

--- src/Components/AddRecipe.js
+++ src/Components/AddRecipe.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import M from "materialize-css/dist/js/materialize.min.js";
 import '../App.css'
+const axios = require('axios');
 
 class AddRecipe extends Component {
   constructor() {
     super();
     this.state = {
-      // ingredients: [],
-      // instructions: []
+      ingredients: [],
+      instructions: []
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
 
   componentDidMount() {
@@ -24,68 +27,77 @@ class AddRecipe extends Component {
     M.Modal.init(elems, options);
     var elems2 = document.querySelectorAll('select');
     M.FormSelect.init(elems2);
-    var elems3 = document.querySelectorAll('.chips');
-    M.Chips.init(elems3);
-    // M.Chips.getInstance('.chips-initial').chipsData();
+    var elems4 = document.querySelectorAll('.chips');
+    M.Chips.init(elems4, {
+        onChipAdd: this.handleTheIStuff,
+        onChipDelete: this.handleTheIStuff,
+        Limit: 10,
+        minLength: 1
+    });  
   }
 
-  handleChange = (e) => {
-    // console.log(e.target.key)
-    this.setState({[e.target.name]: e.target.value}, () => {
-      console.log(this.state);
-    })
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value})
   }
 
   handleTheIStuff = e => {
-    // var data= `$('#mommy').material_chip('data')`;
-    console.log(e);
+    const chipsArray = e[0].M_Chips.chipsData
+    const chip = chipsArray[chipsArray.length - 1].tag
+    let joined;
+
+    if (e[0].className.includes('ingredients')) {
+      joined = this.state.ingredients.concat(chip)
+      this.setState({ ingredients: joined })
+    } else {
+      joined = this.state.instructions.concat(chip)
+      this.setState({ instructions: joined })
+    }
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state)
+    // try {
+    //   const response = await axios.post('http://the-recipes-backend.herokuapp.com/recipe/create', this.state);
+    //   console.log(response)
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
 
   render() {
     return (
       <div id="modal1" className="modal">
         <div className="modal-content">
-          <h4>Add a new recipe</h4>
-            <form className="col s12">
-              <div className='row'>
-                <div className="input-field col s12 mt-4">
-                  <select>
-                    <option value="1">Breakfast</option>
-                    <option value="2">Dinner</option>
-                    <option value="3">Drinks</option>
+          <h4 className='modal-header'>Add a new recipe</h4>
+            <form className="col s12" onSubmit={this.handleSubmit}>
+                <div className="row input-field col s12 mt-4">
+                  <select name='category' onChange={this.handleChange}>
+                    <option value="breakfast">Breakfast</option>
+                    <option value="dinner">Dinner</option>
+                    <option value="drinks">Drinks</option>
                   </select>
-                  <label>Category</label>
                 </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s12">
+                <div className="row input-field col s12">
                   <textarea name='name' onChange={this.handleChange} className="materialize-textarea"></textarea>
-                  <label htmlFor="textarea1">Name</label>
+                  <label>Name</label>
                 </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s12">
+                <div className="row input-field col s12">
                   <textarea name='url' onChange={this.handleChange} className="materialize-textarea"></textarea>
-                  <label htmlFor="textarea1">URL</label>
+                  <label>URL</label>
                 </div>
-              </div>
-              <div className="row">
-                <div className="chips chips-placeholder input-field col s12" chipsData={this.handleTheIStuff}>
-                  <input className='input' name='ingredients' placeholder='Ingredients'></input>
-                  {/* <label htmlFor="textarea1">Ingredients</label> */}
+                <div className="row chips ingredients chips-placeholder input-field col s12">
+                  <input className='input' placeholder='Ingredients'></input>
                 </div>
-              </div>
-              <div className="row">
-                <div className="chips chips-placehodler input-field col s12">
+                <div className="row chips chips-placehodler input-field col s12">
                   <input className="input" placeholder='Instructions'></input>
-                  {/* <label htmlFor="textarea1">Instructions</label> */}
                 </div>
-              </div>
+                <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                  <i className="material-icons right">send</i>
+                </button>
             </form>
         </div>
-        <div className="modal-footer">
-          <a href="#!" className="modal-close waves-effect waves-green btn-flat">Submit</a>
-        </div>
+
       </div>
     )
   }
